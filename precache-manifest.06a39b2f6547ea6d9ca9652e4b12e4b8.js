3e0cc2bff5fa18ff7f555ff3fb44a793f7d50fa8
Prakash Acharya <pracharya2601@gmail.com>
2020-05-05 20:33:01 -0700

    Updates

---
+++ precache-manifest.06a39b2f6547ea6d9ca9652e4b12e4b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e5ac3b7924368b099b2ad6db6be013",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "6c2d0a034340f03f355a",
+    "url": "/clicky-game/static/css/main.80cb61fe.chunk.css"
+  },
+  {
+    "revision": "fbf4adab267f00889a97",
+    "url": "/clicky-game/static/js/2.a152040f.chunk.js"
+  },
+  {
+    "revision": "6c2d0a034340f03f355a",
+    "url": "/clicky-game/static/js/main.e9b38bff.chunk.js"
+  },
+  {
+    "revision": "3c25effdcca3bb9b3162",
+    "url": "/clicky-game/static/js/runtime-main.232931f0.js"
+  }
+]);
